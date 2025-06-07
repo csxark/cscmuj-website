d2275bdd840db87865b13f33de132ea9ea9fbee2
Jatin Kumar Sangewar <sangewarjatinkumar@gmail.com>
2025-06-07 10:23:50 +0530

    ai vs human debate event added

--- data/events.js
+++ data/events.js
@@ -1,21 +1,34 @@
 import errorImage from "../assets/events/error.jpg";
 import battleImage from "../assets/events/battle.jpg";
+import aihumanImage from "../assets/events/aihuman.jpg";
 
 export const events = [
   {
     id: 1,
+    name: "AI vs Human Debate",
+    image: aihumanImage,
+    description:
+      "Step into a high-energy debate on whether Artificial Intelligence or Human Intelligence will shape our future in healthcare, education, cybersecurity, and more! Team up and take a stand to prove which side has the edge. 💥👥",
+    date: "November 15, 2024",
+    location: "Manipal University, AB3 1st Floor",
+    registrationLink: "https://tinyurl.com/3uv5fyaj",
+    tags: ["future", "intelligence", "imagination", "ai showdown"],
+    organizer: "Cyber Space Club",
+  },
+  {
+    id: 2,
     name: "Error Odyssey 2.0",
     image: errorImage,
     description:
       "Join this C programming competition to test your debugging skills! Through three exciting rounds, showcase your ability to find and fix code errors. Perfect for both beginners and experienced programmers.",
     date: "February 18, 2025",
-    location: "Manipal University, AB1 307, 5 Classes",
+    location: "Manipal University, AB1 307",
     registrationLink: "https://forms.gle/DgX3gtmogmJwwtFY8",
     tags: ["competition", "coding", "debugging", "C"],
     organizer: "Cyber Space Club",
   },
   {
-    id: 2,
+    id: 3,
     name: "Battle Blitz 2.0",
     image: battleImage,
     description:
@@ -25,5 +38,5 @@ export const events = [
     registrationLink: "https://forms.gle/KLW59s17Wm68uuuZ7",
     tags: ["social games", "activities", "challenges", "fun"],
     organizer: "Cyber Space Club",
-  },
+  },  
 ];
